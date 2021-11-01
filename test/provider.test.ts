e0722d1f759de358b7bf730361fdd68b81580c09
Ari Palo <ari.palo@almamedia.fi>
2021-11-01 03:52:06 +0200

    test: provider

--- test/provider.test.ts
+++ test/provider.test.ts
@@ -1,6 +1,32 @@
-//import { GithubActionsIdentityProvider, IGithubActionsIdentityProvider } from '../src/provider';
+import * as cdk from '@aws-cdk/core';
+import '@aws-cdk/assert/jest';
+import { GithubActionsIdentityProvider } from '../src/provider';
 
-// TODO
-test('TODO', () => {
-  expect(true).toBe(true);
+const providerArnRegexp = /^arn:aws:iam::\$\{Token\[.+\]\}:oidc-provider\/token\.actions\.githubusercontent\.com$/i;
+
+test('New Provider', () => {
+
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app);
+  new GithubActionsIdentityProvider(stack, 'GithubProvider');
+
+  expect(stack).toHaveResource('Custom::AWSCDKOpenIdConnectProvider', {
+    ClientIDList: [
+      'sts.amazonaws.com',
+    ],
+    ThumbprintList: [
+      'a031c46782e6e6c662c2c87c76da9aa62ccabd8e',
+    ],
+    Url: 'https://token.actions.githubusercontent.com',
+  });
+});
+
+test('Existing Provider', () => {
+
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app);
+  const provider = GithubActionsIdentityProvider.fromAccount(stack, 'GithubProvider');
+
+  expect(provider.openIdConnectProviderIssuer).toBe('token.actions.githubusercontent.com');
+  expect(provider.openIdConnectProviderArn).toMatch(providerArnRegexp);
 });
