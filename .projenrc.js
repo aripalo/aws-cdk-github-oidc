ce198f263ad0b3417faa75bb84a7ee2d322e9073
Ari Palo <ari@aripalo.fi>
2022-11-08 14:10:55 +0200

    build: projen config refactor and use npm

--- .projenrc.js
+++ .projenrc.js
@@ -1,32 +1,53 @@
-const { awscdk } = require('projen');
+const { awscdk, github, TextFile, javascript } = require('projen');
+
+const nodejsVersion = '14.21.1';
+
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Ari Palo',
-  authorAddress: 'opensource@aripalo.com',
-  cdkVersion: '2.12.0',
 
-
-  /**
-   * we default release the main branch(cdkv2) with major version 2.
-   */
-  majorVersion: 2,
-  defaultReleaseBranch: 'main',
-  /**
-   * we also release the cdkv1 branch with major version 1.
-   */
-  releaseBranches: {
-    cdkv1: { npmDistTag: 'cdkv1', majorVersion: 1 },
-  },
-
+  // Metadata
   stability: 'experimental',
+  authorName: 'Ari Palo',
+  authorOrganization: false,
+  authorAddress: 'opensource@aripalo.com',
   name: 'aws-cdk-github-oidc',
   description: 'CDK constructs to use OpenID Connect for authenticating your Github Action workflow with AWS IAM',
   repositoryUrl: 'https://github.com/aripalo/aws-cdk-github-oidc.git',
   keywords: ['cdk', 'aws-cdk', 'awscdk', 'aws', 'iam', 'github', 'github-actions', 'oidc', 'openid-connect'],
 
-  devDeps: ['@types/github-username-regex', 'constructs'],
-  // release: undefined,              /* Add release management to this project. */
+  // Publish configuration
+  defaultReleaseBranch: 'main',
+  majorVersion: 2, // we default release the main branch(cdkv2) with major version 2.
+  releaseBranches: {
+    // we also release the cdkv1 branch with major version 1
+    cdkv1: { npmDistTag: 'cdkv1', majorVersion: 1 },
+  },
+  packageManager: javascript.NodePackageManager.NPM,
+  npmAccess: javascript.NpmAccess.PUBLIC,
+  publishToPypi: {
+    distName: 'aws-cdk-github-oidc',
+    module: 'aws_cdk_github_oidc',
+  },
+  publishToGo: {
+    moduleName: 'github.com/aripalo/aws-cdk-github-oidc-go',
+  },
 
-  gitignore: ['.DS_Store'],
+  // Dependencies
+  minNodeVersion: nodejsVersion,
+  cdkVersion: '2.24.1',
+  constructsVersion: '10.0.0',
+  peerDeps: ['constructs', 'aws-cdk-lib'],
+  devDeps: ['@types/github-username-regex', 'constructs'],
+  bundledDeps: [],
+
+  // Gitignore
+  gitignore: [
+    '.DS_Store',
+    '/examples/**/cdk.context.json',
+    '/examples/**/node_modules',
+    '/examples/**/cdk.out',
+    '/examples/**/.git',
+    '**/*.drawio.bkp',
+  ],
 
   tsconfig: {
     compilerOptions: {
@@ -34,15 +55,12 @@ const project = new awscdk.AwsCdkConstructLibrary({
     },
   },
 
-  publishToPypi: {
-    distName: 'aws-cdk-github-oidc',
-    module: 'aws_cdk_github_oidc',
-  },
-
-  publishToGo: {
-    moduleName: 'github.com/aripalo/aws-cdk-github-oidc-go',
-  },
 
   codeCov: true,
 });
+
+new TextFile(project, '.nvmrc', {
+  lines: [nodejsVersion],
+});
+
 project.synth();
